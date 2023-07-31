b08b54500d8087d6a5a610139ff529093817520f
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-07-31 17:18:48 +0500

    Update showmore.js

--- showmore.js
+++ showmore.js
@@ -13,3 +13,37 @@ $('.show_more').on('click', function () {
         }
     });
 });
+
+
+
+// ye code kya https://syedhussainabbaszaidi.github.io/
+
+$('.col3_main>div').each(function(i,v){
+  if(i<12){
+      $(v).addClass('portfolio_show');
+  } 
+});
+
+$('.col3_btn>a').on('click',function(){
+  let child_length = $('.col3_main>.portfolio_show').length + 4;
+  console.log(child_length);
+  $('.col3_main>div').removeClass('portfolio_show');
+  $('.col3_main>div').each(function(i,v){
+      if(i<child_length){
+        $(v).addClass('portfolio_show');
+      }
+  });
+});
+
+// style.css
+
+.col3_box {
+    position: relative;
+    display: none;
+    width: 22%;
+    transition: .4s;
+}
+
+.portfolio_show{
+    display: block;
+}
