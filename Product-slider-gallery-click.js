732d6f446c6e963a7b6b03f93debb52213d5055d
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-10-09 12:25:53 +0500

    Update Product-slider-gallery-click.js

--- Product-slider-gallery-click.js
+++ Product-slider-gallery-click.js
@@ -1,1 +1,5 @@
-p
+//product slider click k lye media par
+$('.product-gallery__thumbnail-list > .product-gallery__thumbnail:nth-child(1)').addClass('is-active');
+$('.custom_arrow > .next').on('click',function(){
+   $('.product-gallery__thumbnail-list > .product-gallery__thumbnail.is-active').next('button').addClass('is-active').prev('button').removeClass('is-active');
+});
