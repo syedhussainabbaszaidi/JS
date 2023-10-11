5b97b3860037514a515f6d229039a6379d561055
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-10-11 19:18:14 +0500

    Create Oneclickthis.js

---
+++ Oneclickthis.js
@@ -0,0 +1,8 @@
+$('.col3_box_1 > .col3_slide:not(:first-child)').hide();
+$('.col3_bottom > h6').on('click',function(){
+	let child = ($(this).index()) + 1;
+	$(this).parent().find('h6').removeClass('active');
+	$(this).addClass('active');
+	$('.col3_box.col3_box_1>div').hide();
+	$('.col3_box.col3_box_1>div:nth-child('+child+')').show();
+});
