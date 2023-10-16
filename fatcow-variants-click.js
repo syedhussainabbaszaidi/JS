10f5b3a2eb7412734fdce1806dd2eeb34d2aa56a
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-10-16 18:59:29 +0500

    Update fatcow-variants-click.js

--- fatcow-variants-click.js
+++ fatcow-variants-click.js
@@ -1,3 +1,58 @@
+<div id="custom_qty">
+<div class="quantity_heading">Quantity</div>
+<label class="box_qty active" data-discount="1">
+  <span>1</span>
+  <input type="radio" name="custom_qty" value="1" checked="checked">
+</label>
+<label class="box_qty"  data-discount="2">
+  <span>2</span>
+  <img src="https://cdn.shopify.com/s/files/1/0619/7752/6429/files/10_percent.png?v=1684244498">
+  <input type="radio" name="custom_qty" value="2">
+</label>
+<label class="box_qty" data-discount="3">
+  <span>3</span>
+  <img src="https://cdn.shopify.com/s/files/1/0619/7752/6429/files/15_percent.png?v=1684244498">
+  <input type="radio" name="custom_qty" value="3">
+</label>
+<!--                 <label class="box_qty">
+  <span>4</span>
+  <img src="https://cdn.shopify.com/s/files/1/0619/7752/6429/files/40.png?v=1682581634">
+  <input type="radio" name="custom_qty" value="4">
+</label> -->
+</div>
+// quantity change horhi or price pr change horhi hen click k throw  data-price add karai h liquid k throw
+$('#custom_qty input').on('change', function () {
+  $('.quantity__input>option').removeAttr('selected')
+  $('.quantity__input>option:nth-child('+$(this).val()+')').attr('selected', 'selected');
+});
+
+$(document).on('click', '.box_qty', function(){
+  $(this).parent().find('.box_qty').removeClass('active');
+  $(this).addClass('active');
+
+  if($(this).data('discount') == 2) {
+    let a = $('.main_varaints>.variant-option.active').attr('data-price');
+    console.log(a);
+    a = a.trim();
+    a = parseFloat(a.replace('£','')).toFixed(2);
+    var b = "£"+(a-(a*0.10));
+    $('.price__regular .price-item.price-item--regular').text(b);
+  }
+  else if($(this).data('discount') == 3) {
+    let a = $('.main_varaints>.variant-option.active').attr('data-price');
+    console.log(a);
+    a = a.trim();
+    a = parseFloat(a.replace('£','')).toFixed(2);
+    var b = "£"+(a-(a*0.15)).toFixed(2);
+    $('.price__regular .price-item.price-item--regular').text(b);
+  }
+  else {
+    let a = $('.main_varaints>.variant-option.active').attr('data-price');
+    $('.price__regular .price-item.price-item--regular').text("£"+a);
+  }
+  
+});
+
 // quantity par click horha hai or price discount laga rhe
 
 $(window).load(function(){
