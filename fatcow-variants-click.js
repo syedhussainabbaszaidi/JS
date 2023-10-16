8a3f12aa2d802d9f05ecfe70051d5e9af95276ad
Syed Hussain Abbas Zaidi <60025851+syedhussainabbaszaidi@users.noreply.github.com>
2023-10-16 13:45:42 +0500

    Update fatcow-variants-click.js

--- fatcow-variants-click.js
+++ fatcow-variants-click.js
@@ -1,3 +1,34 @@
+// quantity par click horha hai or price discount laga rhe
+
+$(window).load(function(){
+    var orignal_price = $('.price__regular .price-item.price-item--regular').text();
+    console.log("price : "+orignal_price);
+     $('#custom_qty .box_qty:nth-child(2)').on('click',function(){
+      $('.price__regular .price-item.price-item--regular').text(orignal_price);
+      //console.log("This value of orignal_price :" + orignal_price);
+      });
+      $('#custom_qty .box_qty:nth-child(3)').on('click',function(){
+      var a = orignal_price;
+      a = a.trim();
+      a = parseFloat(a.replace('£','')).toFixed(2);
+      //console.log("This value of a :" + a);
+      var b = "£"+(a-(a*0.10));
+      //console.log("This value of b :" + b);
+      $('.price__regular .price-item.price-item--regular').text(b);
+      //console.log("This value of orignal_price :" + orignal_price);
+      });
+      $('#custom_qty .box_qty:nth-child(4)').on('click',function(){
+      var a = orignal_price;
+      a = a.trim();
+      a = parseFloat(a.replace('£','')).toFixed(2);
+      //console.log("This value of a :" + a);
+      var b = "£"+(a-(a*0.15).toFixed(2));
+      //console.log("This value of b :" + b);
+      $('.price__regular .price-item.price-item--regular').text(b);
+      //console.log("This value of orignal_price :" + orignal_price);
+      });
+});
+
 
 // two select option hai or 1 div pr click horha hai
 
